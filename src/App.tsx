b130ddfb489078a6e3622b6ec3ea5082c6381e3a
lorematteo <loremattteo@gmail.com>
2023-08-09 16:06:31 +0200

    build: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
