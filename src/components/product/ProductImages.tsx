a5002828d09aff430009b9eb2fd5531e71139acc
LeLicorne_ <lo.re.baptiste.59@gmail.com>
2023-08-20 21:46:15 +0100

    fix(product): images gap on phone, tablet

--- src/components/product/ProductImages.tsx
+++ src/components/product/ProductImages.tsx
@@ -5,11 +5,11 @@ export default function ProductImages(options: { images: Image[] | undefined }) 
 
   if (!images || images.length === 0) {
     return (
-      <div className="w-full flex flex-col gap-5">
+      <div className="w-full flex flex-col gap-3 lg:gap-5">
         <div className=" w-full object-cover p-[50px] bg-secondary">
           <img src="/src/assets/products/placeholder.jpg" alt="" />
         </div>
-        <div className="grid grid-cols-4 gap-5 w-full overflow-hidden">
+        <div className="grid grid-cols-4 gap-3 lg:gap-5 w-full overflow-hidden">
           <img src="/src/assets/products/placeholder.jpg" alt="" className=" bg-secondary w-full p-3" />
           <img src="/src/assets/products/placeholder.jpg" alt="" className=" bg-secondary w-full p-3" />
           <img src="/src/assets/products/placeholder.jpg" alt="" className=" bg-secondary w-full p-3" />
