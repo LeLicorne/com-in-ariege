0cc03faab8474ea6aa27817392eae016f95a16ee
lorematteo <loremattteo@gmail.com>
2023-08-21 14:03:40 +0200

    feat(admin): updated nav

--- src/admin/navigation/Store.tsx
+++ src/admin/navigation/Store.tsx
@@ -1,8 +1,11 @@
 import { BiStore } from 'react-icons/bi';
+import { useNavigate } from 'react-router-dom';
 
 export default function Store() {
+  const nav = useNavigate();
   return (
     <button
+      onClick={() => nav('/')}
       className="w-[200px] h-9 inline-flex items-center text-sm font-medium transition-colors border rounded-md bg-white hover:bg-secondary px-3 gap-4"
       type="button"
     >
