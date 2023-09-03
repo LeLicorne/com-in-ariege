ec6b88b819ec64a5d1505006132fba616a708670
lorematteo <loremattteo@gmail.com>
2023-09-03 15:19:24 +0200

    feat(contact): call booking back

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,7 +1,23 @@
 import { useEffect, useState } from 'react';
-import { FaCheck, FaCross, FaPaperPlane } from 'react-icons/fa';
+import { FaCheck, FaPaperPlane } from 'react-icons/fa';
 import { TailSpin } from 'react-loader-spinner';
 import { useSendMessageMutation } from '../redux/api';
+
+const FaX = () => {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth="0"
+      viewBox="0 0 384 512"
+      height="20px"
+      width="20px"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+    </svg>
+  );
+};
 
 export function Input(options: {
   title: string;
@@ -43,7 +59,7 @@ export function Input(options: {
               </p>
             ) : isError ? (
               <p className="flex flex-row gap-3 text-error">
-                Erreur ! <FaCross size={20} />
+                Erreur ! <FaX />
               </p>
             ) : (
               <p className="flex flex-row gap-3">
